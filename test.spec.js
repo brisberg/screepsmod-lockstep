79f0a31a0f59bc13e89d6be39d43f4ab58bf3f39
brisberg <brandon.risberg@gmail.com>
2020-06-17 12:11:26 -0700

    Extracted a helper function to start Screeps Server

--- test.spec.js
+++ test.spec.js
@@ -2,20 +2,26 @@
 const path = require('path');
 const cp = require('child_process');
 
+// Forks a full Screeps Server process and returns the handle
+function forkServerProcess() {
+  const execPath = path.resolve(
+      path.dirname(require.resolve('@screeps/launcher')),
+      '../bin/screeps.js',
+  );
+  return cp.fork(
+      path.resolve(execPath),
+      ['start', '--steam_api_key', process.env.STEAM_API_KEY],
+      {cwd: path.join(__dirname, './testEnv'), stdio: 'inherit'},
+  );
+}
+
 describe('ScreepsMod Lockstep', () => {
   it('should launch a private server with the mod enabled', async () => {
-    const execPath = path.resolve(
-        path.dirname(require.resolve('@screeps/launcher')),
-        '../bin/screeps.js',
-    );
-    const serverProc = cp.fork(
-        path.resolve(execPath),
-        ['start', '--steam_api_key', process.env.STEAM_API_KEY],
-        {cwd: path.join(__dirname, './testEnv'), stdio: 'inherit'},
-    );
+    const serverProc = forkServerProcess();
 
     await new Promise((resolve) => setTimeout(resolve, 4000));
     serverProc.kill();
+
     expect(true).toBeTruthy();
   });
 });
