51b6d0b183dea12787ba9dae58448229167a5ebe
brisberg <brandon.risberg@gmail.com>
2020-06-18 22:09:28 -0700

    Added more tests for initialization, and pubsub response

--- test/test.spec.js
+++ test/test.spec.js
@@ -13,12 +13,23 @@ describe('ScreepsMod Lockstep', () => {
     serverProc.kill();
   })
 
-  it('should launch a private server with the mod enabled', async () => {
+  it.only('server should be paused on startup', async () => {
+    expect(await env.get(LOCKSTEP_COUNT)).toEqual(0);
+  });
+
+  it('should progress two ticks when LOCKSTEP_COUNT set to 2', async () => {
     const startTime = await env.get(env.keys.GAMETIME);
     await env.set(LOCKSTEP_COUNT, 2);
     await new Promise((resolve) => setTimeout(resolve, 3000));
 
-    expect(await env.get(env.keys.GAMETIME)).toEqual(startTime + 2)
+    expect(await env.get(env.keys.GAMETIME)).toEqual(startTime + 2);
     expect(await env.get(LOCKSTEP_COUNT)).toEqual(0);
+  });
+
+  it('should post over Pubsub when server becomes locked', async () => {
+    await env.set(LOCKSTEP_COUNT, 2);
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    // expect(pubsub.subscribe).toBe('locked');
   });
 });
