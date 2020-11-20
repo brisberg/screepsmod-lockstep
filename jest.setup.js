2d5e971fa9a4ecefe348329a1a71971b4540b6d5
brisberg <brandon.risberg@gmail.com>
2020-11-19 21:16:55 -0800

    Adding rebuild to the test script

--- jest.setup.js
+++ jest.setup.js
@@ -1,3 +1,4 @@
 // set up screeps testing environment variables
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const dotenv = require('dotenv');
 dotenv.config({path: './screeps-testing-env'});
